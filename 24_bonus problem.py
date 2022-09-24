d18fb66ebe45cd89f3daf8363cacc947222317dd
HEEL PATEL <91087524+sanatani07@users.noreply.github.com>
2022-09-25 03:14:13 +0530

    Update 24_bonus problem.py

--- 24_bonus problem.py
+++ 24_bonus problem.py
@@ -1,3 +1,10 @@
+'''
+A company decides to give bonus to their employees on this Diwali.
+#1 A '5%' bonus on salary is given to the male workers and '10%' bonus salary to the female workers.
+#2 If the salary of the emaployee is less than 15000 then the employee get an extra '3%' bonus on the salary
+#3 WAP to enter your salary and gender then calculate the bonus that has to given to the employee and display the salary that the employee will get.
+'''
+
 sal=int(input('enter your salary'))
 gen=input('enter your gender')
 
